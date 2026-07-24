e528bffe2fb137d1132cf03ccadac8f657971a96
bralm7479-eng <bralm7479@gmail.com>
2026-07-24 10:29:49 +0700

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,15 @@
+const SUPABASE_URL = "https://skzujzzzbbezclwzafoh.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_TBcfRBo7Yl8OPkJvw4q_Xg_lvRLIGCy";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);const SUPABASE_URL = "https://skzujzzzbbezclwzafoh.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_TBcfRBo7Yl8OPkJvw4q_Xg_lvRLIGCy";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
